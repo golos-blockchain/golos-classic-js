1736104b8ec5e13e8ba7437eda5e6c6f31c2ad68
1aerostorm <aerostorm1@gmail.com>
2021-08-04 03:29:41 +0300

    0.8.4, fix reputation, add HF 26 chain props

--- src/formatter.js
+++ src/formatter.js
@@ -156,7 +156,7 @@ module.exports = steemAPI => {
       rep = neg ? rep.substring(1) : rep;
       const str = rep;
       const leadingDigits = parseInt(str.substring(0, 4));
-      const log = Math.log(leadingDigits) / Math.log(10);
+      const log = Math.log(leadingDigits) / Math.LN10 + 0.00000001;
       const n = str.length - 1;
       let out = n + (log - parseInt(log));
       if (isNaN(out)) out = 0;
